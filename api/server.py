35a22603149fb46d4a5485cecd76c8b4f6b3dd22
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 12:31:53 +0200

    Update server.py

--- api/server.py
+++ api/server.py
@@ -1,11 +1,11 @@
 from http.server import BaseHTTPRequestHandler
-
+from main import getSponsorCount
 class handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','text/plain')
         self.end_headers()
-        message = ('Hello from Python from a Serverless Function!')
+        message = getSponsorCount()
         self.wfile.write(message.encode())
         return
