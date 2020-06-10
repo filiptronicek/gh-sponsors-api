58c5e190b3ec16253609802f08d85f332e3a4180
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 11:40:26 +0200

    Add to functiom

--- main.py
+++ main.py
@@ -7,15 +7,16 @@ usr = "yg"
 url = f'https://github.com/sponsors/{usr}'
 resp = req.get(url)
 
-if resp.history:
-    sponsors = None
-else:
-    htmlGH = BeautifulSoup(resp.text, 'html.parser')
-    count = htmlGH.select_one("div.border-top:nth-child(2) > p:nth-child(1)").get_text()
+def getSponsorCount():
+    if resp.history:
+        sponsors = None
+    else:
+        htmlGH = BeautifulSoup(resp.text, 'html.parser')
+        count = htmlGH.select_one("div.border-top:nth-child(2) > p:nth-child(1)").get_text()
 
-    for txt in count.split(" "):
-        x = re.search("[0-9]", txt)
+        for txt in count.split(" "):
+            x = re.search("[0-9]", txt)
 
-        if x:
-            sponsors = int(txt)
-print(sponsors)
+            if x:
+                sponsors = int(txt)
+    return sponsors
