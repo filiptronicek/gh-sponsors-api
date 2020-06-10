8fe676fa61e22679a91569de81a64966bda8e412
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 23:47:22 +0200

    Headers

--- api/main.py
+++ api/main.py
@@ -28,6 +28,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         message = getSponsorCount(self.requestline)
         self.wfile.write(str(message).encode())
