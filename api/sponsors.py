8422a0d2a5d4a9db65f71dcbf3d78de4748cedf5
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 12:41:20 +0200

    Add to seperate files

--- api/sponsors.py
+++ api/sponsors.py
@@ -8,24 +8,24 @@ usr = "yg"
 url = f'https://github.com/sponsors/{usr}'
 resp = req.get(url)
 
-def getSponsorCount():
+
+def getSponsorNames():
     if resp.history:
         sponsors = None
     else:
         htmlGH = BeautifulSoup(resp.text, 'html.parser')
-        count = htmlGH.select_one("div.border-top:nth-child(2) > p:nth-child(1)").get_text()
-
-        for txt in count.split(" "):
-            x = re.search("[0-9]", txt)
-            if x:
-                sponsors = int(txt)
-    return json.dumps({"count":sponsors})
-
+        count = htmlGH.select("div.mr-1 > a > img")
+        users = []
+        for handle in count:
+            users.append({"handle": handle['alt'].replace('@', ''),"avatar": handle['src']})
+        d = users
+        d = json.dumps(d)
+        return d
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
         self.end_headers()
-        message = getSponsorCount()
+        message = getSponsorNames()
         self.wfile.write(str(message).encode())
         return
