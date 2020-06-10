b7ea51f517ccad37d51f132d4393facb76e698b7
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 12:59:53 +0200

    Add profile page URL

--- api/sponsors.py
+++ api/sponsors.py
@@ -17,7 +17,7 @@ def getSponsorNames():
         count = htmlGH.select("div.mr-1 > a > img")
         users = []
         for handle in count:
-            users.append({"handle": handle['alt'].replace('@', ''),"avatar": handle['src']})
+            users.append({"handle": handle['alt'].replace('@', ''),"avatar": handle['src'], "profile": "https://github.com/"+handle['alt']})
         d = users
         d = json.dumps(d)
         return d
