0912441c088bba020be9b3a4e2ebd029af8ae6a0
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 11:39:09 +0200

    Basic logic

---
+++ main.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests as req
+import re
+
+usr = "yg"
+
+url = f'https://github.com/sponsors/{usr}'
+resp = req.get(url)
+
+if resp.history:
+    sponsors = None
+else:
+    htmlGH = BeautifulSoup(resp.text, 'html.parser')
+    count = htmlGH.select_one("div.border-top:nth-child(2) > p:nth-child(1)").get_text()
+
+    for txt in count.split(" "):
+        x = re.search("[0-9]", txt)
+
+        if x:
+            sponsors = int(txt)
+print(sponsors)
