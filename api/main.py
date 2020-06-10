de6b6026a79355a6bc993b39bbb86c9aabe3a482
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-10 12:38:39 +0200

    Update main.py

--- api/main.py
+++ api/main.py
@@ -36,8 +36,8 @@ def getSponsorNames():
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
-        self.send_header('Content-type','text/plain')
+        self.send_header('Content-type','application/json')
         self.end_headers()
-        message = getSponsorCount()
+        message = getSponsorNames()
         self.wfile.write(str(message).encode())
         return
