3f46a2416f0fc05990b826c102ae318cbcd39dcc
Filip Troníček <filip.tronicek@seznam.cz>
2020-06-11 11:24:04 +0200

    Headers in Sponsors

--- api/sponsors.py
+++ api/sponsors.py
@@ -29,6 +29,7 @@ class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.send_header('Content-type','application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         message = getSponsorNames(self.requestline)
         self.wfile.write(str(message).encode())
