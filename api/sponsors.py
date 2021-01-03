7f07bc7a569b8db31b5cb32293dfca4ea365f0f7
Filip Troníček <filip.tronicek@seznam.cz>
2021-01-03 14:58:48 +0100

    Add auto pagination (#5)
    * Add auto pagination
    
    * Details on <10 users
    
    * Don't reset var
    
    * api req to JSON

--- api/sponsors.py
+++ api/sponsors.py
@@ -10,31 +10,39 @@ if environ.get("gh_token") is None:
     from dotenv import load_dotenv
     load_dotenv()
 
-
-
 def getUsrDetails(u):
-   # headers = {"Authorization": f'token {getenv("gh_token")}'}
+    headers = {"Authorization": f'token {getenv("gh_token")}'}
     reqs = req.get("https://api.github.com/users/"+u).text
-    return reqs
+    return json.loads(reqs)
 
 def getSponsorNames(u: str):
     usr = u.split("?u=")[1].split("HTTP")[0].replace(" ", "")
-    url = f'https://github.com/sponsors/{usr}'
-    resp = req.get(url)
-    sponsors = 0
-    if resp.history:
-        sponsors = None
-    else:
-        htmlGH = BeautifulSoup(resp.text, 'html.parser')
-        count = htmlGH.select("div.mr-1 > a > img")
-        users = []
-        for handle in count:
-            handle['alt'] = handle['alt'].replace('@', '')
-            users.append({"handle": handle['alt'],"avatar": handle['src'], "profile": "https://github.com/"+handle['alt'], "details" : json.loads(getUsrDetails(handle['alt']))})
-        d = users
-        d = json.dumps(d)
-    if sponsors == None:
-        d = "Eror: GitHub Sponsors aren't setup with this user."
+    totalUsers = []
+    for i in range(1, 1000):
+        url = f'https://github.com/sponsors/{usr}/sponsors_partial?page={i}'
+        resp = req.get(url)
+        usrCount = 0
+        sponsors = 0
+        if resp.history:
+            sponsors = None
+        else:
+            htmlGH = BeautifulSoup(resp.text, 'html.parser')
+            count = htmlGH.select("div.mr-1 > a > img")
+
+            for handle in count:
+                usrCount += 1
+                handle['alt'] = handle['alt'].replace('@', '')
+                if len(count) > 10:
+                    totalUsers.append({"handle": handle['alt'],"avatar": handle['src'], "profile": "https://github.com/"+handle['alt']})
+                else:
+                    totalUsers.append({"handle": handle['alt'],"avatar": handle['src'], "profile": "https://github.com/"+handle['alt'], "details": getUsrDetails(handle['alt'])})
+
+            if usrCount == 0:
+                break
+
+        if sponsors == None:
+            d = "Eror: GitHub Sponsors aren't setup with this user."
+    d = json.dumps(totalUsers)
     return  '{"sponsors": '+d+"}"
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
